c9156e270973d9cb1e3dbc4e4955fa32c6393cad
shernio <steven.hernio@niji.fr>
2022-05-06 10:39:42 +0200

    Commit pour conflit

--- GUIHandler.py
+++ GUIHandler.py
@@ -2,6 +2,7 @@
 
 def initGUI(self):
     # We don't need any of this stuff if we're not running the GUI.
+    #Je modifie cette ligne avec modification B
     self.textqueue = queue.Queue()
     self.progressqueue = queue.Queue()
     root = Tk()
